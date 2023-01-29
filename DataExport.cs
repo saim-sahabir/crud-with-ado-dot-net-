4fc7f79412a6615ec1ec45914102e4ba6f077d40
saim sahabir <87026727+saim-sahabir@users.noreply.github.com>
2023-01-30 00:22:09 +0600

    Update DataExport.cs
    update

--- DataExport.cs
+++ DataExport.cs
@@ -42,3 +42,45 @@ public FileResult ExportToExcel()
             sheetData.Append(dataRow);
             }
 }
+    
+    
+    
+    
+    
+    
+    
+    
+ /////   defrent way 
+    
+    
+        public BrandsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public ActionResult ExportToExcel()
+    {
+        List<Brand> brands = _context.Brands.ToList();
+        byte[] result = CreateExcelFile(brands);
+        return File(result, "application/ms-excel", "Brands.xlsx");
+    }
+
+    private byte[] CreateExcelFile(List<Brand> brands)
+    {
+        using (var package = new ExcelPackage())
+        {
+            var worksheet = package.Workbook.Worksheets.Add("Brands");
+            worksheet.Cells[1, 1].Value = "ID";
+            worksheet.Cells[1, 2].Value = "Name";
+            worksheet.Cells[1, 3].Value = "Entry Date";
+            int row = 2;
+            foreach (var brand in brands)
+            {
+                worksheet.Cells[row, 1].Value = brand.Id;
+                worksheet.Cells[row, 2].Value = brand.Name;
+                worksheet.Cells[row, 3].Value = brand.EntryDate;
+                row++;
+            }
+            return package.GetAsByteArray();
+        }
+    }
