c350ab0f2f2116adbb05a52b7329228b6022229b
saim sahabir <87026727+saim-sahabir@users.noreply.github.com>
2023-01-30 01:23:14 +0600

    Update CsvExport.cs

--- CsvExport.cs
+++ CsvExport.cs
@@ -23,3 +23,35 @@ public class BrandsController : Controller
         return File(fileBytes, "text/csv", "Brands.csv");
     }
 }
+
+
+
+
+
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using System.IO;
+using System.Text;
+
+public class BrandsController : Controller
+{
+    private YourDbContext db = new YourDbContext();
+
+    public ActionResult GenerateAndSaveCsvFile()
+    {
+        var brands = db.Brands.ToList();
+        var sb = new StringBuilder();
+        sb.AppendLine("ID,Name,Entry Date");
+        foreach (var brand in brands)
+        {
+            sb.AppendLine($"{brand.Id},{brand.Name},{brand.EntryDate}");
+        }
+        var fileName = "Brands.csv";
+        var filePath = Path.Combine(Server.MapPath("~/Reports"), fileName);
+        System.IO.File.WriteAllText(filePath, sb.ToString());
+
+        return File(filePath, "text/csv", fileName);
+    }
+}
+
