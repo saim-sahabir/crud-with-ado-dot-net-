77248fcf67112120b88fbfa9efa8f14b3b59610f
saim sahabir <87026727+saim-sahabir@users.noreply.github.com>
2023-01-30 00:55:12 +0600

    Update WordFileExport.cs

--- WordFileExport.cs
+++ WordFileExport.cs
@@ -57,3 +57,79 @@ public class BrandsController : Controller
         return cell;
     }
 }
+
+
+////ohter way 
+
+
+
+
+using System.Data.Entity;
+using System.Linq;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+public class BrandsController : Controller
+{
+    private YourDbContext db = new YourDbContext();
+
+    public ActionResult GenerateWordDocument()
+    {
+        var brands = db.Brands.ToList();
+
+        using (var document = WordprocessingDocument.Create("Brands.docx", WordprocessingDocumentType.Document))
+        {
+            var mainPart = document.AddMainDocumentPart();
+            mainPart.Document = new Document();
+            mainPart.Document.AppendChild(new Body());
+
+            var table = new Table();
+            var tableProperties = new TableProperties();
+            var tableStyle = new TableStyle() { Val = "Table Grid" };
+            tableProperties.AppendChild(tableStyle);
+            table.AppendChild(tableProperties);
+
+            var tableRow = new TableRow();
+
+            var idCell = new TableCell();
+            idCell.AppendChild(new Paragraph(new Run(new Text("ID"))));
+            tableRow.AppendChild(idCell);
+
+            var nameCell = new TableCell();
+            nameCell.AppendChild(new Paragraph(new Run(new Text("Name"))));
+            tableRow.AppendChild(nameCell);
+
+            var entryDateCell = new TableCell();
+            entryDateCell.AppendChild(new Paragraph(new Run(new Text("Entry Date"))));
+            tableRow.AppendChild(entryDateCell);
+
+            table.AppendChild(tableRow);
+
+            foreach (var brand in brands)
+            {
+                tableRow = new TableRow();
+
+                idCell = new TableCell();
+                idCell.AppendChild(new Paragraph(new Run(new Text(brand.Id.ToString()))));
+                tableRow.AppendChild(idCell);
+
+                nameCell = new TableCell();
+                nameCell.AppendChild(new Paragraph(new Run(new Text(brand.Name))));
+                tableRow.AppendChild(nameCell);
+
+                entryDateCell = new TableCell();
+                entryDateCell.AppendChild(new Paragraph(new Run(new Text(brand.EntryDate.ToString()))));
+                tableRow.AppendChild(entryDateCell);
+
+                table.AppendChild(tableRow);
+            }
+
+            mainPart.Document.Body.AppendChild(table);
+            document.Save();
+        }
+
+        return File("Brands.docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "Brands.docx");
+    }
+}
+
