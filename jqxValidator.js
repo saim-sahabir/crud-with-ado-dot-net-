4a7f04f81a8edb4fb21bce81fcbb6899fdeb0629
saim sahabir <87026727+saim-sahabir@users.noreply.github.com>
2023-02-02 23:39:41 +0600

    Update jqxValidator.js

--- jqxValidator.js
+++ jqxValidator.js
@@ -27,3 +27,41 @@ In this example, we first initialize the jqxValidator plugin on the form with id
 
 
 
+
+$("#myform").jqxValidator({
+  rules: [
+    {
+      input: "#username",
+      message: "Username is required!",
+      action: "keyup, blur",
+      rule: "required"
+    },
+    // additional rules...
+  ],
+  ajax: true,
+  submit: function() {
+    $.ajax({
+      url: "path/to/server",
+      type: "post",
+      data: $("#myform").serialize(),
+      success: function(response) {
+        if (response.status == "error") {
+          $.each(response.errors, function(field, message) {
+            $("#myform").jqxValidator("addRule", {
+              input: "#" + field,
+              message: message,
+              action: "keyup, blur",
+              rule: "required"
+            });
+          });
+          $("#myform").jqxValidator("validate");
+        } else {
+          // success
+        }
+      }
+    });
+  }
+});
+
+
+In this example, we first initialize the jqxValidator plugin on the form with id="myform" and set the rules property to specify the validation rules for individual form fields. The ajax property is set to true to indicate that an AJAX request should be made to the server when the form is submitted. In the submit function, we make an AJAX request to the server and pass the form data using $("#myform").serialize(). If the response from the server indicates an error, we use the $.each() function to loop through the response.errors object and add error messages to the corresponding form fields using the jqxValidator("addRule", ...) method. If the response indicates success, we can perform any desired action.
