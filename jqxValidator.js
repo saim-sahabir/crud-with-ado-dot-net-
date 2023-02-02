68dce21f8087266baf7fd691f6dff2081528525f
saim sahabir <87026727+saim-sahabir@users.noreply.github.com>
2023-02-02 23:36:41 +0600

    Create jqxValidator.js

---
+++ jqxValidator.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("#myform").jqxValidator({
+    rules: [
+      { input: "#username", message: "Username is required", action: "keyup, blur", rule: "required" },
+      { input: "#password", message: "Password is required", action: "keyup, blur", rule: "required" }
+    ],
+    ajax: true,
+    ajaxSettings: {
+      url: "path/to/server",
+      type: "post"
+    },
+    onSuccess: function (response) {
+      if (response.status == "error") {
+        $.each(response.errors, function (field, message) {
+          $("#myform").jqxValidator("addError", field, message);
+        });
+      } else {
+        // success
+      }
+    }
+  });
+});
+
+
+
+In this example, we first initialize the jqxValidator plugin on the form with id="myform". The rules option defines the validation rules for the form fields. The ajax option is set to true to indicate that an AJAX request will be made to the server for validation. The ajaxSettings option specifies the URL and type of the request. The onSuccess option is called when the AJAX request is successful and receives the response from the server. If the response indicates an error, we use the $.each() function to loop through the response.errors object and add the error messages to the form fields using the jqxValidator("addError", field, message) method. If the response indicates success, we can perform any desired action.
+
+
+
