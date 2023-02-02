f46f4586eb6d489f23396f76117cba9803594859
saim sahabir <87026727+saim-sahabir@users.noreply.github.com>
2023-02-02 23:20:26 +0600

    Update excelDonwloadwithAjax.js

--- excelDonwloadwithAjax.js
+++ excelDonwloadwithAjax.js
@@ -20,3 +20,21 @@ $.ajax({
 });
 
 
+
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "path/to/excel/file", true);
+xhr.responseType = "blob";
+xhr.onload = function(e) {
+  if (this.status === 200) {
+    let blob = new Blob([this.response], { type: "application/vnd.ms-excel" });
+    let link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "file.xls";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+};
+xhr.send();
+
+
